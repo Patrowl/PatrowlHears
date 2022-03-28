3d522c6ac8f921885bdb5b8be2a93b6573b3181b
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2022-03-28 10:10:54 +0000

    fix import

--- backend_app/backend_app/adfs.py
+++ backend_app/backend_app/adfs.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from django.conf import settings
 from django.contrib.auth import authenticate
+from rest_framework.response import Response
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
